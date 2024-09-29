5837f0d5a19d256a3f2ce49879d4f5401185b097
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-09-29 15:16:18 +0200

    feat: introduce the new private route to react-router-dom

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -5,17 +5,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/Login/SignUp";
+import Editor from "./Components/Editor/Editor";
 
 export function App() {
   return (
-    <div className="h-screen w-screen flex flex-col gap-12 pt-4 pb-8">
+    <div className="min-h-screen w-screen flex flex-col gap-12 pt-4 pb-8 relative">
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="docs" element={<Editor />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="signin" element={<Login />} />
         </Routes>
       </BrowserRouter>
+
+      <footer className="w-full h-7 bg-gray-800 text-center text-white absolute bottom-0">
+        <p>© 2024 DocuFlow. All Rights Reserved.</p>
+      </footer>
     </div>
   );
 }
