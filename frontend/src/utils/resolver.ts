da9cdf0fdaebef54785f566d169e6c94a53380d6
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-09-29 15:15:33 +0200

    chore: remove console

--- frontend/src/utils/resolver.ts
+++ frontend/src/utils/resolver.ts
@@ -4,28 +4,17 @@ const resolveStringArrConflict = (
 ): string => {
   shorter.forEach((se, si) => {
     const inBoth = longer.findIndex((le) => le === se);
-    console.log("inBoth: ", inBoth);
     if (inBoth === -1) {
-      console.log("not in both");
-      console.log("si: ", si);
-      console.log("se: ", se);
-
       longer.splice(si, 0, se);
     }
   });
   const merged = longer.join(" ");
-  console.log("merged: ", merged);
   return merged;
 };
 
 export const mergeString = (firstString: string, secondString: string) => {
   const firstArray = firstString.split(" ").filter(Boolean);
   const secondArray = secondString.split(" ").filter(Boolean);
-
-  console.log("firstArray: ", firstArray);
-  console.log("secondArray: ", secondArray);
-  console.log("first len: ", firstArray.length);
-  console.log("second len: ", secondArray.length);
 
   const isFirstL = firstArray.length >= secondArray.length;
 
